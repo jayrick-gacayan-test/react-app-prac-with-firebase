ffc4a06e5c4d687759f9f3aad45ac0926feffeb3
jayrick-gacayan-test <jayrick-gacayan@kodakollectiv.com>
2022-04-20 14:05:05 +0800

    Add some text in App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import hello from './img_hello_react.jpg';
 const App = () => {
   return (
     <div>
+      <h1>React Tutorial</h1>
       <img  src={ hello } 
             alt="Hello React"
             style={{ height: "600px"}} />
-      First time using ReactJS
+        <p>First the using react.</p>
     </div>
   );
 }
